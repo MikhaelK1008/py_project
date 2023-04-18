c865735326f6e8ec78bbf5e419f8b5ce039040ca
MikhaelK1008 <kamenskie_mm@mail.ru>
2023-04-18 21:50:23 +0300

    second commit

--- funcs.py
+++ funcs.py
@@ -6,6 +6,12 @@ def summ_number():
     return a + b
 print(f'summ is {summ_number()}')
 
-
+def minus_number():
+    print(f'insert first number')
+    a = int(input())
+    print(f'insert second number')
+    b = int(input())
+    return a - b
+print(f'summ is {summ_number()}')
 
 
